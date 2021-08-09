5509ef406bab2626c534eba2e2b533dc7cfd4245
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-08-10 01:41:40 +0530

    Create Floyd Warshall.cpp

---
+++ Floyd Warshall.cpp
@@ -0,0 +1,38 @@
+	void shortest_distance(vector<vector<int>>&matrix){
+	    
+	    int n = matrix.size();
+	    for(int i =0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	            if(matrix[i][j]==-1)
+	              matrix[i][j]=INT_MAX/2;
+	        }
+	    }
+	    
+	    for(int k=0;k<n;k++)
+	    {
+	       for(int i =0;i<n;i++)
+	       {
+	        for(int j=0;j<n;j++)
+	        {
+	            
+	              matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+	        }
+	       }
+	    }
+	    for(int i =0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	            if(matrix[i][j]==INT_MAX/2)
+	              matrix[i][j]=-1;
+	        }
+	    }
+	}
+  
+  	    //to detect negative cycle
+	    for(int i=0;i<n;i++){
+	        if(matrix[i][i]<0){
+	            cout<<"\n Its a negative weight cycle";
+	        }
