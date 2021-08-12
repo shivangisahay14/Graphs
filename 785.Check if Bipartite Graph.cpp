cdde0f8f2be75f2fda825d4b3090c7c4e76958db
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-08-12 19:15:55 +0530

    Update 785.Check if Bipartite Graph.cpp

--- 785.Check if Bipartite Graph.cpp
+++ 785.Check if Bipartite Graph.cpp
@@ -1,4 +1,62 @@
- bool checkComponentsForBipartite(vector<vector<int>> &graph, int &src, vector<int>  &isVisited){
+ bool checkComponents(vector<vector<int>>& graph,int V,int &src,int vis[]){
+        
+        queue<pair<int,int>>q;
+        q.push({src,0}); //source,level
+        
+        while(!q.empty()){
+            pair<int,int>p=q.front();
+            int i=p.first;
+            int j=p.second;
+            q.pop();
+            
+            if(vis[i]!=-1){
+                if(j!=vis[i]){
+                    return false;
+                }
+            }
+            else{
+                vis[i]=j;
+            }
+            
+            for(int x:graph[i]){
+                if(vis[x]==-1){
+                    q.push({x,j+1});
+                }
+            }
+            
+        }
+        return true;
+        
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        int V=graph.size();
+        int vis[V];
+	    fill(vis,vis+V,-1);
+	    
+	    for(int v=0;v<V;v++){
+	        if(vis[v]==-1){
+	            bool ans= checkComponents(graph,V,v,vis);
+	            if(ans==false){
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+        
+    }
+
+
+
+
+
+
+
+
+
+
+bool checkComponentsForBipartite(vector<vector<int>> &graph, int &src, vector<int>  &isVisited){
         queue <pair<int, int> > q;
         q.push(make_pair(src, 0));
         while(!q.empty()){
